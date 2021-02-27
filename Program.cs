705fb8896782b7462ab5c719dd52214f1cca0164
lutfiqaraman <lutfiqaraman@gmail.com>
2021-02-27 22:22:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMessageWriter writer = new ConsoleMessageWriter();
+
+            Salutation salutation = new Salutation(writer);
+            salutation.Exclaim();
+
+            Console.ReadKey();
+        }
+    }
+}
